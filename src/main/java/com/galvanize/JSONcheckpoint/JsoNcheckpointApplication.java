9282512e4bd35c52fb6083bc5728d20a4fecc160
cdub020 <63630268+cdub020@users.noreply.github.com>
2020-07-22 07:27:18 -0700

    initial commit

---
+++ src/main/java/com/galvanize/JSONcheckpoint/JsoNcheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.JSONcheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoNcheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoNcheckpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/JSONcheckpoint/JsoNcheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.JSONcheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoNcheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
